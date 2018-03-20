3e3616c069fef62e7a4729c2f015f175ed065aee
stop2stare <emitremmus0235@163.com>
2018-03-20 22:38:24 +0800

    make sure math.add does not have any side effects

--- 4-demo/test.js
+++ 4-demo/test.js
@@ -1,6 +1,9 @@
 const assert = require('assert')
 const math = require('./')
 
+const factor1 = 1
+const factor2 = 2
+
 describe('math', () => {
   describe('add', () => {
     it('should throw when params are not number type', () => {
@@ -9,7 +12,11 @@ describe('math', () => {
       }, TypeError)
     })
     it('should add when two numbers passed', () => {
-      assert.equal(math.add(1, 2), 3)
+      assert.equal(math.add(factor1, factor2), 3)
+
+      // make sure math.add doesn't have any side effects
+      assert.equal(factor1, 1)
+      assert.equal(factor2, 2)
     })
   })
 
@@ -20,7 +27,9 @@ describe('math', () => {
       }, TypeError)
     })
     it('should minus when two numbers passed', () => {
-      assert.equal(math.minus(1, 2), -1)
+      assert.equal(math.minus(factor1, factor2), -1)
+      assert.equal(factor1, 1)
+      assert.equal(factor2, 2)
     })
   })
 })
